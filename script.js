a5260af8dfb818aa98206919ef1afdf1be3d67ff
EvelinaBruzhas <evelina.delovan@gmail.com>
2022-07-26 15:52:56 +0300

    footer

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let center = [48.8866527839977,2.34310679732974];
+
+function init() {
+	let map = new ymaps.Map('map-test', {
+		center: center,
+		zoom: 17
+	});
+
+	map.controls.remove('geolocationControl'); 
+  map.controls.remove('searchControl'); 
+  map.controls.remove('trafficControl'); 
+  map.controls.remove('typeSelector'); 
+  map.controls.remove('fullscreenControl'); 
+  map.controls.remove('zoomControl'); 
+  map.controls.remove('rulerControl'); 
+  map.behaviors.disable(['scrollZoom']);
+}
+
+ymaps.ready(init);
